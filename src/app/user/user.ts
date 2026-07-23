d54498bd1fc57ad56b091dc59cb5379042704212
Miruna Agopian <agopian.miruna@yahoo.com>
2026-07-23 21:33:43 +0200

    created GET-function to display users from firebase

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, afterNextRender } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,6 +8,14 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { UserModel } from '../../models/user.class';
 import { Firestore } from '@angular/fire/firestore';
 import { MatCardModule } from '@angular/material/card';
+// import { collection, collectionData } from '@angular/fire/firestore';
+
+
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../../environments/environment';
+import { RouterLink } from "@angular/router";
+import { log } from 'firebase/firestore/pipelines';
 
 @Component({
   selector: 'app-user',
@@ -18,14 +26,27 @@ import { MatCardModule } from '@angular/material/card';
     MatDialogModule,
     MatNativeDateModule,
     MatCardModule,
-  ],
+    RouterLink
+],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
 export class User {
   user: UserModel = new UserModel();
   dialog = inject(MatDialog);
-  firestore: Firestore = inject(Firestore);
+  allUsers: any[] = [];
+  firestore = getFirestore(initializeApp(environment.firebase));
+
+  async ngOnInit() {
+    const usersRef = collection(this.firestore, 'users');
+    const snapshot = await getDocs(usersRef);
+
+    this.allUsers = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+  }
+
 
   openDialog() {
     this.dialog.open(DialogAddUser);
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+bootstrapApplication(App, appConfig)
+  .catch(err => console.error('Bootstrap error:', err));
 
-import { environment } from './environments/environment';
-
-bootstrapApplication(App, {
-  ...appConfig,
-  providers: [
-    ...(appConfig.providers ?? []),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
-  ]
-})
-.catch(err => console.error('Bootstrap error:', err));
